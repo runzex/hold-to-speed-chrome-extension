18453e7d0d3ab8509eda813b36a1e81bb22b0acc
RunZe Xu <run@RunMacBook.local>
2026-02-20 14:50:32 +1100

    added new features

--- constants.js
+++ constants.js
@@ -5,7 +5,10 @@ const DEFAULT_SETTINGS = {
   increaseKeyCode: "BracketRight",
   decreaseKeyCode: "BracketLeft",
   resetKeyCode: "Backquote",
+  seekBackwardKeyCode: "Comma",
+  seekForwardKeyCode: "Period",
   speedStep: 0.25,
+  seekStepSeconds: 5,
   hudX: 20,
   hudY: 20,
 };
